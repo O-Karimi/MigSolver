faf163f42432628b9b8f828c9ee3f2006b62cd38
O-Karimi <omidkarimi54@gmail.com>
2026-03-09 05:07:11 +0100

    feat: Creating users and categories in data base
    - Base user created
    - Base categories created

--- basics/models.py
+++ basics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # AbstractUser automatically provides: username, email, password, and date_joined!
+    # We are keeping this empty for now with 'pass', but setting it up this way 
+    # means we can easily add custom fields (like 'avatar' or 'bio') in Version 2.0.
+    pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, help_text="The URL-friendly version of the name.")
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories" # Fixes a typo in the Django Admin so it doesn't say "Categorys"
+
+    def __str__(self):
+        return self.name
