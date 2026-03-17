cbe3f76a2ca088797dc8d00e854ff9e6fb3c3d0e
O-Karimi <omidkarimi54@gmail.com>
2026-03-17 11:09:02 +0100

    feat(wiki): implement wiki article model and category junction

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from basics.models import Category
 
-# Create your models here.
+class WikiArticle(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True) # Auto-updates every time you save!
+    
+    # Notice we use SET_NULL instead of CASCADE here. 
+    # If an Admin leaves the team and their account is deleted, 
+    # we don't want the database to automatically delete all their Wiki articles!
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, 
+        null=True,
+        related_name='wiki_articles'
+    )
+    
+    categories = models.ManyToManyField(Category, related_name='wiki_articles')
+
+    def __str__(self):
+        return self.title
