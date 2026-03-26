002752102f01e1124c23d7f2bb0df9df1eaeaa44
O-Karimi <omidkarimi54@gmail.com>
2026-03-26 15:53:05 +0100

    Updating Mock data creation autorun!

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# NEW: The Safety Switch
+if [ "$SEED_MOCK_DATA" = "True" ]; then
+    echo "Safety switch is ON. Seeding mock data..."
+    python manage.py setup_mock_data
+else
+    echo "Skipping mock data generation."
+fi
